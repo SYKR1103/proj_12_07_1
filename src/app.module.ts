09a5433b1b15c353121a7a0c9f300ffa5f9d1918
sun <thebthebtheb1234@gmail.com>
2023-12-07 17:54:06 +0900

    db settting..

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DblistModule } from './dblist/dblist.module';
+import { AppConfigModule } from './config/Appconfig.module';
+import { ConfigModule } from "@nestjs/config";
+import * as Joi from '@hapi/joi'
 
 @Module({
-  imports: [],
+  imports: [AppConfigModule, DblistModule, AppConfigModule
+],
   controllers: [AppController],
   providers: [AppService],
 })
